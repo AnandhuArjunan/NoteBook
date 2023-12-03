710d49220860fc4476dd756d57397f7c687d28f8
Anandhu Arjunan <anandhuka97@gmail.com>
2023-12-03 15:22:17 +0530

    bug fix

--- src/main/java/com/anandhuarjunan/notesfx/views/MainPresenter.java
+++ src/main/java/com/anandhuarjunan/notesfx/views/MainPresenter.java
@@ -212,11 +212,11 @@ public class MainPresenter implements Initializable {
 		minimize.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
 			stage.setIconified(true);
 		});
-		wHeader.setOnMousePressed(event -> {
+		root.setOnMousePressed(event -> {
 			xOffset = stage.getX() - event.getScreenX();
 			yOffset = stage.getY() - event.getScreenY();
 		});
-		wHeader.setOnMouseDragged(event -> {
+		root.setOnMouseDragged(event -> {
 			stage.setX(event.getScreenX() + xOffset);
 			stage.setY(event.getScreenY() + yOffset);
 		});
